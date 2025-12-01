c26858c8c4f87209fd4e15e00b503784edc128cc
burak <burakisbakan521998@gmail.com>
2025-12-01 12:55:56 +0300

    burak

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,26 +1,56 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-
     public Animation Camera;
+    public Image blackScreen;    // Fade için siyah ekran
+    public float fadeTime = 1.5f;
+
+    void Start()
+    {
+        blackScreen.gameObject.SetActive(false);
+    }
 
     public void NewGame()
     {
+        blackScreen.gameObject.SetActive(true);
+        StartCoroutine(FadeAndStart());
+    }
+
+    IEnumerator FadeAndStart()
+    {
+        float t = 0f;
+
+        // Yavaşça karart
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Lerp(0, 1, t / fadeTime);
+            blackScreen.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        // Sonra sahneyi yükle
         Application.LoadLevel(2);
     }
+
     public void LoadGame()
     {
 
     }
+
     public void Settings()
     {
         Camera.Play("Menu2Settings");
     }
+
     public void BacktoMenu()
     {
         Camera.Play("Settings2Menu");
     }
+
     public void Exit()
     {
         Application.Quit();
