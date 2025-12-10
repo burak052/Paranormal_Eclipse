3354fe8df1aa0278738c44d3c173826f6a8e2b7d
burak <burakisbakan521998@gmail.com>
2025-12-10 21:14:59 +0300

    burak

--- Assets/Scripts/CharacterScripts/Raycast.cs
+++ Assets/Scripts/CharacterScripts/Raycast.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Raycast : MonoBehaviour
 {
     [Header("Raycast Ayarları")]
-    public float interactDistance = 3f;  // Kapıya yaklaşma mesafesi
-    public LayerMask layerMask;          // "Door" layerı varsa ekleyin (zorunlu değil)
+    public float interactDistance = 3f;  
+    public LayerMask layerMask;          
 
     [Header("UI")]
-    public GameObject pressEUI;          // "Press E" UI'si
+    public GameObject pressEUI;          
     public GameObject EnviroKeypad;
     private Animator currentDoorAnimator;
+    public MonoBehaviour playerMovement;
+
+    public TextMeshProUGUI passwordText;
+    public string password;
+    public bool pressenter;
 
     void Start()
     {
         if (pressEUI != null)
             pressEUI.SetActive(false);
+        if (EnviroKeypad != null)
+            EnviroKeypad.SetActive(false);
+        pressenter = false;
+        passwordText.text = "";
     }
 
     void Update()
@@ -23,20 +33,15 @@ public class Raycast : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        // Raycast gönder
         if (Physics.Raycast(ray, out hit, interactDistance, layerMask.value == 0 ? ~0 : layerMask))
         {
-            // Objeyi kontrol et
             if (hit.collider.CompareTag("BoilerDoor"))
             {
-                // UI görünür olsun
                 if (pressEUI != null)
                     pressEUI.SetActive(true);
 
-                // Animator referansı al
                 currentDoorAnimator = hit.collider.GetComponent<Animator>();
 
-                // E tuşuna basılırsa animasyon tetikle
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     if (currentDoorAnimator != null)
@@ -49,25 +54,124 @@ public class Raycast : MonoBehaviour
             }
             if (hit.collider.CompareTag("EnviroKeypad"))
             {
+                if(EnviroKeypad != null && EnviroKeypad.activeSelf)
+                {
+                    if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || pressenter || Input.GetKeyDown(KeyCode.Backspace))
+                    {
+                        if(Input.GetKeyDown(KeyCode.Return) || pressenter)
+                        {
+                            
+                            if(passwordText.text == password)
+                            {   
+                                EnviroKeypad.SetActive(false);
+                                currentDoorAnimator = hit.collider.transform.parent.Find("Up").GetComponent<Animator>();
+                                if (currentDoorAnimator != null)
+                                {
+                                bool state = currentDoorAnimator.GetBool("Open");
+                                    currentDoorAnimator.SetBool("Open", !state);
+                                }
+                                currentDoorAnimator = hit.collider.transform.parent.Find("Down").GetComponent<Animator>();
+                                if (currentDoorAnimator != null)
+                                {
+                                    bool state = currentDoorAnimator.GetBool("Open");
+                                    currentDoorAnimator.SetBool("Open", !state);
+                                }
+                                currentDoorAnimator = hit.collider.transform.parent.Find("Down").Find("Middle").GetComponent<Animator>();
+                                if (currentDoorAnimator != null)
+                                {
+                                    bool state = currentDoorAnimator.GetBool("Open");
+                                    currentDoorAnimator.SetBool("Open", !state);
+                                }
+                                pressenter = false;
+                                EnviroKeypad.SetActive(false);
+                                
+                                playerMovement.enabled = true;
+
+                                Cursor.visible = false;
+                                Cursor.lockState = CursorLockMode.Locked;
+                            }
+                            else
+                            {
+                                ClearKey();
+                                pressenter = false;
+                            }
+                        }
+
+                        if(Input.GetKeyDown(KeyCode.Escape))
+                        {
+                            EnviroKeypad.SetActive(false);
+                            
+                            playerMovement.enabled = true;
+
+                            Cursor.visible = false;
+                            Cursor.lockState = CursorLockMode.Locked;
+                        }
+
+                        if(Input.GetKeyDown(KeyCode.Backspace))
+                        {
+                            ClearKey();
+                        }
+                    }
+                                        
+                    // if(passwordText.text.Length <= 3)
+                    // {
+                    //     if(Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+                    //         passwordText.text = passwordText.text + "0";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+                    //         passwordText.text = passwordText.text + "1";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+                    //         passwordText.text = passwordText.text + "2";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+                    //         passwordText.text = passwordText.text + "3";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+                    //         passwordText.text = passwordText.text + "4";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+                    //         passwordText.text = passwordText.text + "5";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+                    //         passwordText.text = passwordText.text + "6";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+                    //         passwordText.text = passwordText.text + "7";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+                    //         passwordText.text = passwordText.text + "8";
+                    //     if(Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+                    //         passwordText.text = passwordText.text + "9";
+                    // }
+                }
+
                 if (pressEUI != null)
                     pressEUI.SetActive(true);
 
-              
-
-                // E tuşuna basılırsa animasyon tetikle
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     if (EnviroKeypad != null)
                         EnviroKeypad.SetActive(true);
+                    playerMovement.enabled = false;
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
                 }
                 return;
             }
         }
 
-        // Raycast bir şey bulamazsa UI kapanır
         if (pressEUI != null)
             pressEUI.SetActive(false);
 
         currentDoorAnimator = null;
     }
+
+    public void KeyButton(string key)
+    {        
+        if (passwordText.text.Length <= 3)
+            passwordText.text = passwordText.text + key;
+    }
+    
+    public void ClearKey()
+    {
+        passwordText.text = "";
+    }    
+
+    public void EnterKey()
+    {
+        pressenter = true;
+    }   
 }
